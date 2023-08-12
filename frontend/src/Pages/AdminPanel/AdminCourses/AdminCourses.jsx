921ca94e3068b4b9762db83661231c131447d8d8
Ali <ali.tajik.1385.a@gmail.com>
2023-08-12 18:22:47 +0330

    adding new course

--- frontend/src/Pages/AdminPanel/AdminCourses/AdminCourses.jsx
+++ frontend/src/Pages/AdminPanel/AdminCourses/AdminCourses.jsx
@@ -2,10 +2,49 @@ import React, { useEffect, useState } from 'react'
 import "./AdminCourses.css"
 import Table from '../../../Components/AdminPanel/Table/Table'
 import swal from 'sweetalert'
+import { useForm } from "./../../../hooks/useForm";
+import Input from "./../../../Components/Form/Input/Input";
+import {
+    requiredValidator,
+    minValidator,
+    maxValidator,
+} from "./../../../Validator/rules";
+import "./AdminCourses.css";
 
 export default function AdminCourses() {
     const [courses, setCourses] = useState([])
     const localStorageData = JSON.parse(localStorage.getItem("user"))
+    const [courseCategory, setCourseCategory] = useState("");
+    const [categories, setCategories] = useState([]);
+    const [courseStatus, setCourseStatus] = useState('start')
+    const [courseCover, setCourseCover] = useState({})
+
+    const [formState, onInputHandler] = useForm(
+        {
+            name: {
+                value: "",
+                isValid: false,
+            },
+            description: {
+                value: "",
+                isValid: false,
+            },
+            shortName: {
+                value: "",
+                isValid: false,
+            },
+            price: {
+                value: "",
+                isValid: false,
+            },
+            support: {
+                value: "",
+                isValid: false,
+            },
+        },
+        false
+    );
+
     function getAllCourses() {
         fetch("http://localhost:4000/v1/courses")
             .then(res => res.json())
@@ -16,6 +55,11 @@ export default function AdminCourses() {
     }
     useEffect(() => {
         getAllCourses()
+        fetch(`http://localhost:4000/v1/category`)
+            .then((res) => res.json())
+            .then((allCategories) => {
+                setCategories(allCategories);
+            });
     }, [])
     const deleteCourse = (coursesId) => {
         swal({
@@ -46,8 +90,154 @@ export default function AdminCourses() {
                 }
             })
     }
+    const selectCategory = (event) => {
+        setCourseCategory(event.target.value);
+    };
+    const addNewCourse = event => {
+        event.preventDefault()
+        console.log(formState);
+    }
     return (
         <>
+            <div class="container-fluid" id="home-content">
+                <div class="container">
+                    <div class="home-title">
+                        <span>افزودن دوره جدید</span>
+                    </div>
+                    <form class="form">
+                        <div class="col-6">
+                            <div class="name input">
+                                <label class="input-title">نام دوره</label>
+                                <Input
+                                    id="name"
+                                    element="input"
+                                    onInputHandler={onInputHandler}
+                                    validations={[minValidator(5)]}
+                                    type="text"
+                                    placeholder="لطفا نام دوره را وارد کنید..."
+                                />
+                                <span class="error-message text-danger"></span>
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <div class="price input">
+                                <label class="input-title">توضیحات دوره</label>
+                                <Input
+                                    id="description"
+                                    element="input"
+                                    onInputHandler={onInputHandler}
+                                    validations={[minValidator(5)]}
+                                    type="text"
+                                    placeholder="لطفا توضیحات دوره را وارد کنید..."
+                                />
+                                <span class="error-message text-danger"></span>
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <div class="number input">
+                                <label class="input-title">Url دوره</label>
+                                <Input
+                                    id="shortName"
+                                    element="input"
+                                    onInputHandler={onInputHandler}
+                                    validations={[minValidator(5)]}
+                                    type="text"
+                                    isValid="false"
+                                    placeholder="لطفا Url دوره را وارد کنید..."
+                                />
+                                <span class="error-message text-danger"></span>
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <div class="price input">
+                                <label class="input-title">قیمت دوره</label>
+                                <Input
+                                    id="price"
+                                    element="input"
+                                    onInputHandler={onInputHandler}
+                                    validations={[minValidator(5)]}
+                                    type="text"
+                                    isValid="false"
+                                    placeholder="لطفا قیمت دوره را وارد کنید..."
+                                />
+                                <span class="error-message text-danger"></span>
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <div class="price input">
+                                <label class="input-title">نحوه پشتیبانی دوره</label>
+                                <Input
+                                    id="support"
+                                    element="input"
+                                    onInputHandler={onInputHandler}
+                                    validations={[minValidator(5)]}
+                                    type="text"
+                                    isValid="false"
+                                    placeholder="لطفا نحوه پشتیبانی دوره را وارد کنید..."
+                                />
+                                <span class="error-message text-danger"></span>
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <div class="number input">
+                                <label class="input-title">دسته‌بندی دوره</label>
+                                <select onChange={selectCategory}>
+                                    {categories.map((category) => (
+                                        <option value={category._id}>{category.title}</option>
+                                    ))}
+                                </select>
+                                <span class="error-message text-danger"></span>
+                            </div>
+                        </div>
+                        <div class="col-6">
+                            <div class="file">
+                                <label class="input-title">عکس دوره</label>
+                                <input type="file" id="file" onChange={event => {
+                                    console.log(event.target.files[0]);
+                                    setCourseCover(event.target.files[0])
+                                }} />
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="bottom-form">
+                                <div class="condition">
+                                    <label class="input-title">وضعیت دوره</label>
+                                    <div class="radios">
+                                        <div class="available">
+                                            <label>
+                                                <span>در حال برگزاری</span>
+                                                <input
+                                                    type="radio"
+                                                    value="start"
+                                                    name="condition"
+                                                    checked
+                                                    onInput={event => setCourseStatus(event.target.value)}
+                                                />
+                                            </label>
+                                        </div>
+                                        <div class="unavailable">
+                                            <label>
+                                                <span>پیش فروش</span>
+                                                <input
+                                                    type="radio"
+                                                    value="presell"
+                                                    name="condition"
+                                                    onInput={event => setCourseStatus(event.target.value)}
+                                                />
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="submit-btn">
+                                    <input type="submit" value="افزودن" onClick={addNewCourse} />
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+
             <Table title=" دوره ها">
                 <table class="table">
                     <thead>
