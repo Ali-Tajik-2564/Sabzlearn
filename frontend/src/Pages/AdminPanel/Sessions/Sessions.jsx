8fb14268f76ae4f0ca74c62ad130abbed154c8e9
Ali <ali.tajik.1385.a@gmail.com>
2023-08-16 12:18:58 +0330

    Update Sessions.jsx

--- frontend/src/Pages/AdminPanel/Sessions/Sessions.jsx
+++ frontend/src/Pages/AdminPanel/Sessions/Sessions.jsx
@@ -2,9 +2,13 @@ import React, { useEffect, useState } from "react";
 import { useForm } from "./../../../hooks/useForm";
 import Input from "./../../../Components/Form/Input/Input";
 import { minValidator } from "../../../Validator/rules";
+import Table from "../../../Components/AdminPanel/Table/Table";
+import swal from "sweetalert";
 
 export default function Sessions() {
+    const localStorageData = JSON.parse(localStorage.getItem("user"))
     const [courses, setCourses] = useState([]);
+    const [allSessions, setAllSessions] = useState([])
     const [sessionCourse, setSessionCourse] = useState('-1');
     const [sessionFile, setSessionFile] = useState('-1');
     const [formState, onInputHandler] = useForm(
@@ -28,10 +32,45 @@ export default function Sessions() {
                 console.log(allCourses);
                 setCourses(allCourses);
             });
+        getAllSessions()
     }, []);
+    const getAllSessions = () => {
+        fetch("http://localhost:4000/v1/courses/sessions")
+            .then(res => res.json())
+            .then(SessionsInfo => {
+                setAllSessions(SessionsInfo)
 
+            })
+    }
+    const addNewSession = (event) => {
+        event.preventDefault()
+        let fromNewSession = new FormData()
+        fromNewSession.append("title", formState.inputs.title.value)
+        fromNewSession.append("time", formState.inputs.time.value)
+        fromNewSession.append("video", sessionFile)
 
+        fetch(`http://localhost:4000/v1/courses/${sessionCourse}/sessions`, {
+            method: "POST",
+            headers: {
+                "Authorization": `Bearer ${localStorageData}`
+            }
+            , body: fromNewSession
+        })
+            .then(res => {
+                if (res.ok) {
 
+                    res.json()
+                }
+            }
+            )
+            .then(result => {
+                swal({
+                    title: "دوره با موفقیت اپدیت شد"
+                    , icon: "success"
+                    , buttons: "ok"
+                })
+            })
+    }
     return (
         <>
             <div class="container-fluid" id="home-content">
@@ -87,7 +126,7 @@ export default function Sessions() {
                                 <label class="input-title" style={{ display: "block" }}>
                                     فایل مربوطه
                                 </label>
-                                <input type="file" onChange={(event) => sessionFile(event.target.files.item[0])} />
+                                <input type="file" onChange={(event) => setSessionFile(event.target.files.item[0])} />
                                 <span class="error-message text-danger"></span>
                             </div>
                         </div>
@@ -95,13 +134,51 @@ export default function Sessions() {
                         <div class="col-12">
                             <div class="bottom-form">
                                 <div class="submit-btn">
-                                    <input type="submit" value="افزودن" />
+                                    <input type="submit" value="افزودن" onClick={event => addNewSession(event)} />
                                 </div>
                             </div>
                         </div>
                     </form>
                 </div>
             </div>
+            <Table title=" جلسات">
+                <table class="table">
+                    <thead>
+
+                        <tr>
+                            <th>شناسه</th>
+                            <th>عنوان</th>
+                            <th>تایم</th>
+                            <th>حذف</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+
+                        {allSessions.map((sessions, index) => (
+                            <tr>
+                                <td>{index + 1}</td>
+                                <td>{sessions.title}</td>
+
+                                <td>{sessions.time}</td>
+
+                                <td>
+                                    <button type="button" class="btn btn-danger delete-btn"  >
+                                        حذف
+                                    </button>
+                                </td>
+
+                            </tr>
+                        ))}
+
+
+
+
+
+                    </tbody>
+                </table>
+
+            </Table>
         </>
 
     )
