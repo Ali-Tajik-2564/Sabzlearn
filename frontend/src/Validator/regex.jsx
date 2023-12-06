d3a3960a17d58faecbaa549290454a06cea0289a
Ali <ali.tajik.1385.a@gmail.com>
2023-12-06 08:53:49 +0330

    Update regex.jsx

--- frontend/src/Validator/regex.jsx
+++ frontend/src/Validator/regex.jsx
@@ -2,7 +2,10 @@ const testEmail = (value) => {
   const emailPattern = /^[a-z0-90._-]+@[a-z]+\.[a-z]{2,3}$/g;
   return emailPattern.test(value);
 };
-const testCodeMelli = (value) => {};
+const testCodeMelli = (value) => {
+  const codeMelliPattern = /^\d{10}$/g;
+  return codeMelliPattern.test(value)
+};
 const testPhoneNumber = (value) => {
   const phoneNumberPattern = /^09\d{9}$/g;
   return phoneNumberPattern.test(value);
